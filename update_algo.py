eb7a3bed2d6748e70cd76ec74e7efd17cbffaf64
BenjaminDFH <137965823+BenjaminDFH@users.noreply.github.com>
2024-10-03 11:26:43 -0400

    Update update_algo.py

--- update_algo.py
+++ update_algo.py
@@ -6,6 +6,8 @@
 # I'll name the "allow_list.txt" file as the "import_file" variable.
 import_file = "allow_list.txt"
 
+# list of IP to be removed
+remove_list = []
 
 # Opened the file with "with" statement and .open() to read the contents. "with" help manage the resources by closing the file after exiting the statement. The using the "as" keyword
 # to assign a variable named file storing the output of .open()
@@ -18,4 +20,21 @@ with open(import_file, "r") as file:
 # whitespace into list elements. After splitting the string I save the output back into the "ip_addresses" variable.
 ip_addresses = ip_addresses.split()
 
-# Iterating through the remove list with a for loop
+# Iterating through the remove_list with a for loop 
+for element in remove_list:
+
+    # Cheking if the elements in remove_list are in the IP addresses list
+    if element in ip_addresses:
+
+        # remove with .remove()
+        ip_addresses.remove(element)
+
+# Updating the allow list with the new ip addresses list using .join() to convert it into a string so it can written back into the text file using the .write() method. "\n" instrucst 
+# python to put each element on a new line
+ip_addresses = "\n".join(ip_addresses)
+
+# 'with' statement to write brack to the original file
+with open(import_file, "w") as file:
+
+    # Write to the file the contents of ip_addresses
+    file.write(ip_addresses)
